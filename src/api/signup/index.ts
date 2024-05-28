b7d713d07f61986099c9f4423fbcc2d99c171660
Umar Farooq <131238639+UmarFarooqfr@users.noreply.github.com>
2024-05-28 16:49:00 +0500

    Revert "check again 3"
    This reverts commit 9beba989be3b162cf9699c34fd661735c87b0088.

--- src/api/signup/index.ts
+++ src/api/signup/index.ts
@@ -1,12 +1,16 @@
 import express from 'express';
+const router = express.Router();
 import * as controller from './signUp.controller';
 import { checkToken } from '../../auth/authService';
+const auth = require("../../auth/authService");
 
-const router = express.Router();
+
 
 router.post('/signup', controller.create);
-router.post('/login', controller.loginValidation);
-router.get('/:id/userProfile', checkToken, controller.getUserProfile);
-router.put('/:id/updateUserProfile', checkToken, controller.updateUserProfile);
+router.post('/login',controller.loginValidation)
+router.get('/:id/userProfile', checkToken , controller.getUserProfile);
+router.put('/:id/updateUserProfile', checkToken , controller.updateUserProfile);
 
-export default router;
+
+export = router
+
