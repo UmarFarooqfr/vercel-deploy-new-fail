9beba989be3b162cf9699c34fd661735c87b0088
Umar Farooq <131238639+UmarFarooqfr@users.noreply.github.com>
2024-05-28 16:42:33 +0500

    check again 3

--- src/route.ts
+++ src/route.ts
@@ -1,7 +1,14 @@
-export const routes = function (app: any): void {
-    app.use('/api/user', require('./api/signup'));
-    app.use('/api/UserDevice', require('./api/usersDevices'));
-    app.use('/api/circle', require('./api/circle'));
-    app.use('/api/approvalNotification', require('./api/approvalNotification'));
-    app.use('/api/rejectedUsers', require('./api/rejectedUsers'))
-  }
+import { Express } from 'express';
+import signupRouter from './api/signup';
+// import usersDevicesRouter from './api/usersDevices';
+// import circleRouter from './api/circle';
+// import approvalNotificationRouter from './api/approvalNotification';
+// import rejectedUsersRouter from './api/rejectedUsers';
+
+export const routes = (app: Express): void => {
+  app.use('/api/user', signupRouter);
+  // app.use('/api/UserDevice', usersDevicesRouter);
+  // app.use('/api/circle', circleRouter);
+  // app.use('/api/approvalNotification', approvalNotificationRouter);
+  // app.use('/api/rejectedUsers', rejectedUsersRouter);
+};
