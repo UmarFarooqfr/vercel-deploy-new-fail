35ef2be39d8ebb7e0c7d12d5c665e2748ba9ba39
Umar Farooq <131238639+UmarFarooqfr@users.noreply.github.com>
2024-05-28 17:13:00 +0500

    Revert "check again"
    This reverts commit 86527283c9b677726ee6ea27176d98c27e74aa5d.

--- src/route.ts
+++ src/route.ts
@@ -1,6 +1,7 @@
-import express from 'express';
-import signUpRouter from './api/signup'; // Import the router, not the entire module
-
-export const routes = function (app: express.Application): void {
-    app.use('/api/user', signUpRouter); // Use the router
-}
+export const routes = function (app: any): void {
+    app.use('/api/user', require('./api/signup'));
+    app.use('/api/UserDevice', require('./api/usersDevices'));
+    app.use('/api/circle', require('./api/circle'));
+    app.use('/api/approvalNotification', require('./api/approvalNotification'));
+    app.use('/api/rejectedUsers', require('./api/rejectedUsers'))
+  }
