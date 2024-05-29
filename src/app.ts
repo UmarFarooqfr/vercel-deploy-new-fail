8f62cf4e960408d1f26ac64195d79a941952585b
Umar Farooq <131238639+UmarFarooqfr@users.noreply.github.com>
2024-05-29 17:31:24 +0500

    sdsdsdsd

--- src/app.ts
+++ src/app.ts
@@ -125,10 +125,9 @@ mongoose
   });
 server.listen(port, () => {
   console.log(`Express server listening ${port}`);
-});
- 
-app.get('/', function (req, res) {
-  res.send('**** Hello World! ****');
+  app.get('/', function (req, res) {
+    res.send('**** Hello World! ****');
+  });
 });
 export default app;
 
