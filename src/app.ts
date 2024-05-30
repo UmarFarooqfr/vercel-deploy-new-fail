b2387965c4bc4a97c1d019a30e9d232bb66787ad
Umar Farooq <131238639+UmarFarooqfr@users.noreply.github.com>
2024-05-30 17:43:39 +0500

    asasasasasaaaaaaaaaaa

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,10 @@ import mongoose from "mongoose";
 import http from "http";
 import { routes } from "./route";
 // const socketIo = require('socket.io')
-
+import Ably from 'ably';
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
+const ably = new Ably.Realtime('rAWpPA.WHP_Yg:Q-D8Wsrnd3wk9TNTLYwYyWBQYljuCloNnoJYiY4N0Ck');
 export const app = express();
 const server = http.createServer(app);
 
@@ -18,50 +18,96 @@ const io = require('socket.io')(server, {
 });
 
 export async function requestNotification(name: string, memberName: string, memberId: string): Promise<void> {
-   console.log(`Sending notification to ${name}`);
-    const socketId = connectedUsers.get(memberId);
-  console.log(`Socket ID for user ${name}: ${socketId}`);
-  if (socketId) {
-    io.to(socketId).emit('notification', ` ${name}, ${memberName}! wants to join your circle`);
-   console.log(`Notification sent to ${name}`);
-  } else {
-    console.log(`No socket ID found for user ${name}`);
+  //  console.log(`Sending notification to ${name}`);
+  //   const socketId = connectedUsers.get(memberId);
+  // console.log(`Socket ID for user ${name}: ${socketId}`);
+  // if (socketId) {
+  //   io.to(socketId).emit('notification', ` ${name}, ${memberName}! wants to join your circle`);
+  //  console.log(`Notification sent to ${name}`);
+  // } else {
+  //   console.log(`No socket ID found for user ${name}`);
+  // }
+  const channel = ably.channels.get(`notifications:${memberId}`);
+  try {
+    await channel.publish('notification', ` ${name}, ${memberName}! wants to join your circle`);
+    console.log(`Notification sent to user ${memberId}`);
+  } catch (err) {
+    console.error(`Error sending notification to user ${memberId}:`, err.message);
   }
   }
 
+  // const sendNotificationToUser = async (userId: string, message: string) => {
+  //   const channel = ably.channels.get(`notifications:${userId}`);
+  //   try {
+  //     await channel.publish('notification', { message });
+  //     console.log(`Notification sent to user ${userId}`);
+  //   } catch (err) {
+  //     console.error(`Error sending notification to user ${userId}:`, err.message);
+  //   }
+  // };
+
+
   export async function approvalRequestNotification(name: string, memberName: string, userId: string): Promise<void> {
-     const socketId = connectedUsers.get(userId);
-   if (socketId) {
-     io.to(socketId).emit('notification', ` ${memberName}, ${name}! approve your request to join circle`);
-   } else {
-     console.log(`No socket ID found for user ${name}`);
-   }
+  //    const socketId = connectedUsers.get(userId);
+  //  if (socketId) {
+  //    io.to(socketId).emit('notification', ` ${memberName}, ${name}! approve your request to join circle`);
+  //  } else {
+  //    console.log(`No socket ID found for user ${name}`);
+  //  }
+   const channel = ably.channels.get(`notifications:${userId}`);
+  try {
+    await channel.publish('notification', ` ${memberName}, ${name}! approve your request to join circle`);
+    console.log(`Notification sent to user ${userId}`);
+  } catch (err) {
+    console.error(`Error sending notification to user ${userId}:`, err.message);
+  }
    }
 
    export async function rejectRequestNotification(name: string, memberName: string,userId: string): Promise<void> {
-    const socketId = connectedUsers.get(userId);
-  if (socketId) {
-    io.to(socketId).emit('notification', ` ${memberName} , ${name}! reject your request to join circle`);
-  } else {
-    console.log(`No socket ID found for user ${userId}`);
+  //   const socketId = connectedUsers.get(userId);
+  // if (socketId) {
+  //   io.to(socketId).emit('notification', ` ${memberName} , ${name}! reject your request to join circle`);
+  // } else {
+  //   console.log(`No socket ID found for user ${userId}`);
+  // }
+  const channel = ably.channels.get(`notifications:${userId}`);
+  try {
+    await channel.publish('notification', ` ${memberName}, ${name}!  reject your request to join circle`);
+    console.log(`Notification sent to user ${userId}`);
+  } catch (err) {
+    console.error(`Error sending notification to user ${userId}:`, err.message);
   }
   }
 
   export async function removeUserNotification(name: string, memberName: string,userId: string): Promise<void> {
-    const socketId = connectedUsers.get(userId);
-  if (socketId) {
-    io.to(socketId).emit('notification', ` ${name} ,${memberName}! removed you from circle`);
-  } else {
-    console.log(`No socket ID found for user ${userId}`);
+  //   const socketId = connectedUsers.get(userId);
+  // if (socketId) {
+  //   io.to(socketId).emit('notification', ` ${name} ,${memberName}! removed you from circle`);
+  // } else {
+  //   console.log(`No socket ID found for user ${userId}`);
+  // }
+  const channel = ably.channels.get(`notifications:${userId}`);
+  try {
+    await channel.publish('notification', ` ${memberName}, ${name}!  removed you from circle`);
+    console.log(`Notification sent to user ${userId}`);
+  } catch (err) {
+    console.error(`Error sending notification to user ${userId}:`, err.message);
   }
   }
 
   export async function bannedUserNotification(name: string, memberName: string,userId: string): Promise<void> {
-    const socketId = connectedUsers.get(userId);
-  if (socketId) {
-    io.to(socketId).emit('notification', ` ${name} , ${memberName}! banned you from circle`);
-  } else {
-    console.log(`No socket ID found for user ${userId}`);
+  //   const socketId = connectedUsers.get(userId);
+  // if (socketId) {
+  //   io.to(socketId).emit('notification', ` ${name} , ${memberName}! banned you from circle`);
+  // } else {
+  //   console.log(`No socket ID found for user ${userId}`);
+  // }
+  const channel = ably.channels.get(`notifications:${userId}`);
+  try {
+    await channel.publish('notification', ` ${memberName}, ${name}!  banned you from circle`);
+    console.log(`Notification sent to user ${userId}`);
+  } catch (err) {
+    console.error(`Error sending notification to user ${userId}:`, err.message);
   }
   }
 
@@ -76,37 +122,39 @@ export async function requestNotification(name: string, memberName: string, memb
 const connectedUsers = new Map();
 
 
-io.on('connection', (socket) => {
-  console.log('A user connected:', socket.id);
+// io.on('connection', (socket) => {
+//   console.log('A user connected:', socket.id);
 
-  //io.emit('notification', `Congratulations, sdsdsds! You have successfully logged in.`);
+//   //io.emit('notification', `Congratulations, sdsdsds! You have successfully logged in.`);
 
-  socket.on('register', (data:any) => {
-  console.log(`Register event received for user: ${data}`);
-    connectedUsers.set(data.userId, socket.id);
-    console.log(connectedUsers)
-    console.log(`User registered: ${data.userName} with socket ID: ${socket.id}`);
-  });
+//   socket.on('register', (data:any) => {
+//   console.log(`Register event received for user: ${data}`);
+//     connectedUsers.set(data.userId, socket.id);
+//     console.log(connectedUsers)
+//     console.log(`User registered: ${data.userName} with socket ID: ${socket.id}`);
+//   });
 
 
-  socket.on('registered', (data:any)=>{
-    console.log('hello',data)
-  })
+//   socket.on('registered', (data:any)=>{
+//     console.log('hello',data)
+//   })
   
-  socket.on('connection', ()=>{
-    console.log('hello')
-  })
-  //sendNotification("Fakhar")
-  // socket.on('disconnect', () => {
-  //   for (let [userName, socketId] of connectedUsers.entries()) {
-  //     if (socketId === socket.id) {
-  //       connectedUsers.delete(userName);
-  //       console.log(`User disconnected: ${userName}`);
-  //       break;
-  //     }
-  //   }
-  // });
-});
+//   socket.on('connection', ()=>{
+//     console.log('hello')
+//   })
+//   //sendNotification("Fakhar")
+//   // socket.on('disconnect', () => {
+//   //   for (let [userName, socketId] of connectedUsers.entries()) {
+//   //     if (socketId === socket.id) {
+//   //       connectedUsers.delete(userName);
+//   //       console.log(`User disconnected: ${userName}`);
+//   //       break;
+//   //     }
+//   //   }
+//   // });
+// });
+
+// sendNotificationToUser('sdsdsdsdg','hy mysdsdsd namsdsde is umar')
 
 
 app.use(express.urlencoded({ limit: "150mb", extended: true }));
@@ -130,7 +178,8 @@ app.use((req, res, next) => {
 const port = 3000;
 export const TOKEN_KEY = "11223344";
 routes(app);
-const url = `mailto:mongodb+srv://wwwburjsoft:gbltbwrzsdys9kr5@cluster0.k2bwmju.mongodb.net/?retryWrites=true&w=majority`;
+const url = `mongodb+srv://wwwburjsoft:gBLTbWrZsDYS9kR5@cluster0.k2bwmju.mongodb.net/`;
+// const url = `mailto:mongodb+srv://wwwburjsoft:gbltbwrzsdys9kr5@cluster0.k2bwmju.mongodb.net/?retryWrites=true&w=majority`;
 mongoose
   .connect(url)
   .then(() => {-
@@ -145,7 +194,7 @@ server.listen(port, () => {
 });
 
 app.get('/', function (req, res) {
-  res.send('*** Hello World! ***');
+  res.send('* Hello World! *');
 });
 
 export default app;
